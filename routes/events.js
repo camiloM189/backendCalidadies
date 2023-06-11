d844da6af7d7b7f97928b46d0ac192bc61e0540e
camilo <jcamilomillan18912@gmail.com>
2023-06-11 17:37:45 -0500

    bacc

--- routes/events.js
+++ routes/events.js
@@ -1,15 +1,46 @@
 const {Router} = require('express');
-const { obtenerEventos, crearEventos, actualizarEventos, borrarEventos } = require('../controllers/events');
+const { obtenerEventos, crearEventos, actualizarEventos, borrarEventos, obtenerEventosPorId, getItemList, actualizarItemList, getMetas, actualizarMetas, borrarItemList, borrarMetas, getNotas, borrarNotas, uploadFiles, updateNotes, actualizarTitleItemList } = require('../controllers/events');
 const { validarToken } = require('../middlewares/validarToken');
 const {check} = require('express-validator');
 const { validarCampos } = require('../middlewares/validarCampos');
 const { validarListItem, validarIndicador, validarTitleItem, validarFechaDeInicio, validarFechaDeFinalizacion, validarMetas, validarMetasTitle, validarMetasFechaInicio, validarMetasFechaFinalizacion } = require('../middlewares/validarEventos');
+const multer = require('multer');
+const { storage } = require('../middlewares/uploadMiddleware');
+
+const upload = multer({ storage: storage });
 
 const router = Router()
 
 router.use(validarToken)
 
 router.get('/',obtenerEventos)
+
+router.get('/:id',obtenerEventosPorId)
+
+router.put('/item/:pagina/:id',actualizarItemList)
+
+router.put('/item/:pagina/:id/edit',actualizarTitleItemList)
+
+router.delete('/item/:pagina/:id',borrarItemList)
+
+router.get('/item/:pagina/:id/:_id',getMetas)
+
+router.put('/item/:pagina/:id/:_id',actualizarMetas)
+
+router.delete('/item/:pagina/:id/:_id',borrarMetas)
+
+router.get(`/item/:pagina/:id`,getItemList)
+
+router.get('/item/:pagina/:id/:_id/:noteId',getNotas)
+
+router.put('/item/:pagina/:id/:_id/:noteId',updateNotes)
+
+router.delete('/item/:pagina/:id/:_id/:noteId',borrarNotas)
+
+router.post('/upload',upload.single('file'),uploadFiles);
+
+// router.put('/upload/generate',generateDownloadLink);
+
 
 router.post('/' ,[
         // check('title','El titulo es obligatorio').not().isEmpty(),
